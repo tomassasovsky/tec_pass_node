f67aecd09345d44fe42b295a5e3a5ecd98b8802a
tomassasovsky <to.sasovsky@gmail.com>
2021-08-22 14:25:01 -0300

    messages updated

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -7,8 +7,8 @@ const { validateFields } = require('../middlewares/validate-fields')
 const router = Router()
 
 router.post('/login', [
-  check('email', 'El correo es un campo requerido').isEmail(),
-  check('password', 'La contraseña es un campo requerido').not().isEmpty(),
+  check('email', '· El correo es un campo requerido').isEmail(),
+  check('password', '· La contraseña es un campo requerido').not().isEmpty(),
   validateFields,
 ], login)
 
--- routes/users-routes.js
+++ routes/users-routes.js
@@ -13,20 +13,20 @@ router.get('/', userController.usersGet)
 
 router.put('/:id', [
   validateJWT,
-  check('id', 'El identificador de usuario no es válido').isMongoId(),
+  check('id', '· El identificador de usuario no es válido').isMongoId(),
   check('id').custom(userExists),
   validateFields
 ], userController.usersPut)
 
 router.post('/', [
   // check for errors and return them if there are any:
-  check('name', 'El nombre es obligatorio').notEmpty(),
-  check('email', 'El correo es obligatorio').isEmail(),
+  check('name', '· El nombre es obligatorio').notEmpty(),
+  check('email', '· El correo no es válido').isEmail(),
   check('rut').custom(rutExists),
   check('rut').custom(rutValid),
-  check('phone', 'El teléfono es obligatorio').notEmpty(),
+  check('phone', '· El teléfono es obligatorio').notEmpty(),
   check('phone').custom(phoneExists),
-  check('password', 'La contraseña es obligatoria').isLength({ min: 8 }),
+  check('password', '· La contraseña debe tener por lo menos 8 caracteres').isLength({ min: 8 }),
   check('email').custom(emailExists),
   validateFields
 ], userController.usersPost)
@@ -37,8 +37,8 @@ router.delete('/', [
 ], userController.usersDelete)
 
 router.patch('/', [
-  check('email', 'El correo es obligatorio').isEmail(),
-  check('password', 'El correo es obligatorio').not().isEmpty(),
+  check('email', '· El correo no es válido').isEmail(),
+  check('password', '· El correo es obligatorio').not().isEmpty(),
   validateFields
 ], userController.usersPatch)
 
